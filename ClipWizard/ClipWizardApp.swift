e4773f46e28e456ab651dfb4d8aa9952e5fca48a
Dane Balia <daneb@tuta.io>
2025-03-30 19:34:12 +0200

    Initial Commit

---
+++ ClipWizard/ClipWizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipWizardApp.swift
+//  ClipWizard
+//
+//  Created by Dane Balia on 2025/03/30.
+//
+
+import SwiftUI
+
+@main
+struct ClipWizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipWizardTests/ClipWizardTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipWizardTests.swift
+//  ClipWizardTests
+//
+//  Created by Dane Balia on 2025/03/30.
+//
+
+import Testing
+@testable import ClipWizard
+
+struct ClipWizardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
